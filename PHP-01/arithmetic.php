e10f67799c0c606398bae351c342e3bf5ab99649
Balakirushnan Jashvanth <146808073+jashvanth370@users.noreply.github.com>
2024-07-31 00:18:36 +0530

    Add files via upload

---
+++ PHP-01/arithmetic.php
@@ -0,0 +1,26 @@
+<?php
+
+//Arithmetic operatators 
+// * % + -
+
+$x=10;
+$y=5;
+$p=null;
+
+$z=$x+$y;
+$a=$x/$y;
+$b=$x*$y;
+$c=$x**$y;
+$d=$x%$y;
+
+echo"$z<br>";
+echo"$a<br>";
+echo"$b<br>";
+echo"$c<br>";
+echo"$d<br>";
+echo"$z<br>";
+
+$counter =0;
+$counter++;
+echo"$counter<br>";
+?>
